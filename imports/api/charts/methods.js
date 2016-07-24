895cffe66a656cfdd06ba91e7541f4b93321a39c
PhaniGaddipati <phanigaddipati@gmail.com>
2016-07-24 16:49:47 -0400

    insert chart validated method

--- imports/api/charts/methods.js
+++ imports/api/charts/methods.js
@@ -3,3 +3,36 @@
  */
 import {ValidatedMethod} from "meteor/mdg:validated-method";
 import {SimpleSchema} from "meteor/aldeed:simple-schema";
+import * as Charts from "charts.js";
+
+/**
+ * Inserts a new chart into the database, given the name and description.
+ * A graph is created and associated with the chart automatically.
+ *
+ * The unique _id of the chart is returned, or null on failure.
+ */
+export const insert = new ValidatedMethod({
+    name: 'charts.insert',
+    validate: Charts.Charts.simpleSchema()
+        .pick([Charts.NAME, Charts.DESCRIPTION])
+        .validator({
+            clean: true,
+            filter: true
+        }),
+    run({name, description}){
+        let ownerId = Meteor.userId();
+        if (!ownerId) {
+            throw new Meteor.Error('charts.insert.accessDenied',
+                'A user must be logged in to insert a new Chart');
+        }
+        let graphId = undefined;// TODO create a graph here and insert it with the chart
+
+        let chart                 = {};
+        chart[Charts.OWNER]       = ownerId;
+        chart[Charts.NAME]        = name;
+        chart[Charts.DESCRIPTION] = description;
+        chart[Charts.GRAPH]       = graphId;
+
+        return Charts.Charts.insert(chart);
+    }
+});
